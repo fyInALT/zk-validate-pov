3ff56aca4b72bbace9c4407ca35da51c8b10ff48
fyInALT <fy@altresear.ch>
2023-07-22 23:35:24 +0800

    add pov code and params datas

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,3 +1,4 @@
+use clap::{Arg, ArgAction, Command};
 use methods::{VALIDATE_POV_ELF, VALIDATE_POV_ID};
 use risc0_zkvm::{
     default_executor_from_elf,
@@ -5,18 +6,53 @@ use risc0_zkvm::{
     ExecutorEnv,
 };
 
+fn read_bin_from_file(path: &String) -> Vec<u8> {
+    use std::fs::File;
+    use std::io::Read;
+
+    let mut file = File::open(path).expect("read file failed");
+    let mut contents = Vec::new();
+    file.read_to_end(&mut contents).unwrap();
+    contents
+}
+
 fn main() {
+    // Parse command line
+    let matches = Command::new("validate")
+        .arg(
+            Arg::new("param")
+                .short('p')
+                .action(ArgAction::Append)
+                .required(true),
+        )
+        .arg(Arg::new("debug").short('d').action(ArgAction::SetTrue))
+        .get_matches();
+
+    let log_level = if matches.get_flag("debug") {
+        log::LevelFilter::Debug
+    } else {
+        log::LevelFilter::Info
+    };
+
+    env_logger::builder().filter_level(log_level).init();
+
+    let param_paths: Vec<&String> = matches
+        .get_many::<String>("param")
+        .expect("we should use `-p` to select pov params file!")
+        .collect();
+
+    log::info!("params: {:?}", param_paths);
+
+    let params = param_paths
+        .into_iter()
+        .map(|path| read_bin_from_file(path))
+        .collect::<Vec<_>>();
+
     // First, we construct an executor environment
-    let env = ExecutorEnv::builder().build().unwrap();
-
-    // TODO: add guest input to the executor environment using
-    // ExecutorEnvBuilder::add_input().
-    // To access this method, you'll need to use the alternate construction
-    // ExecutorEnv::builder(), which creates an ExecutorEnvBuilder. When you're
-    // done adding input, call ExecutorEnvBuilder::build().
-
-    // For example:
-    // let env = ExecutorEnv::builder().add_input(&vec).build().unwrap();
+    let env = ExecutorEnv::builder()
+        .add_input(&to_vec(&params).expect("to_vec failed"))
+        .build()
+        .unwrap();
 
     // Next, we make an executor, loading the (renamed) ELF binary.
     let mut exec = default_executor_from_elf(env, VALIDATE_POV_ELF).unwrap();
--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,12 +1,23 @@
 #![no_main]
 // If you want to try std support, also update the guest Cargo.toml file
-#![no_std]  // std support is experimental
+#![no_std] // std support is experimental
 
+extern crate alloc;
 
+use alloc::vec::Vec;
 use risc0_zkvm::guest::env;
 
 risc0_zkvm::guest::entry!(main);
 
 pub fn main() {
     // TODO: Implement your guest code here
+    // Decode the verifying key, message, and signature from the inputs.
+    let params: Vec<Vec<u8>> = env::read();
+
+    // if use the env to pass code from params, it will cost too much times
+    let code = include_bytes!("../../../pov/pov_code");
+
+    // let validate_pov risc0 code contain all code datas.
+    // this will let us to test the costs.
+    env::commit(&code[params.len()]);
 }
