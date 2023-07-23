7034cdb4a032b48bf73a45ab327ccf2a78e44445
fyInALT <fy@altresear.ch>
2023-07-23 17:08:21 +0800

    add a simple impl

---
+++ methods/guest/src/executor.rs
@@ -0,0 +1,30 @@
+// Copyright (C) Alt Research Ltd. All Rights Reserved.
+
+// This source code is licensed under the limited license found in the LICENSE file
+// in the root directory of this source tree.
+
+use alloc::string::ToString;
+use alloc::{vec::Vec};
+
+use scale_codec::Decode;
+// Rollup
+use altbeacon_rollup_executor::{Error, InstantiateError, ReturnError, RuntimeBlob, WasmExecutor};
+use altbeacon_rollup_externalities::ValidationExternalities;
+
+// TODO: add host function
+pub type HostFunctions = ();
+
+/// The wasm method for full validation(substrate style validation) in beacon side.
+pub const BEACON_VALIDATION_METHOD: &str = "beacon_validate_block";
+
+pub fn validate_block(code: &[u8], encoded_params: &[u8]) -> Result<Vec<u8>, Error> {
+    let blob = RuntimeBlob::uncompress_if_needed(code).map_err(|e| InstantiateError::Other(e))?;
+
+    let mut executor = WasmExecutor::<HostFunctions>::new(blob, Some(512), true)?;
+    let mut ext = ValidationExternalities::new();
+
+    match executor.call(&mut ext, BEACON_VALIDATION_METHOD, encoded_params) {
+        Ok(b) => Ok(b),
+        Err(e) => Err(e),
+    }
+}
--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,10 +1,17 @@
+// Copyright (C) Alt Research Ltd. All Rights Reserved.
+
+// This source code is licensed under the limited license found in the LICENSE file
+// in the root directory of this source tree.
+
 #![no_main]
 // If you want to try std support, also update the guest Cargo.toml file
 #![no_std] // std support is experimental
 
+mod executor;
+
 extern crate alloc;
 
-use alloc::vec::Vec;
+use alloc::{format, vec::Vec};
 use risc0_zkvm::guest::env;
 
 risc0_zkvm::guest::entry!(main);
@@ -17,7 +24,8 @@ pub fn main() {
     // if use the env to pass code from params, it will cost too much times
     let code = include_bytes!("../../../pov/pov_code");
 
-    // let validate_pov risc0 code contain all code datas.
-    // this will let us to test the costs.
-    env::commit(&code[params.len()]);
+    let _result = params
+        .into_iter()
+        .map(|param| executor::validate_block(code, &param).expect("validate_block failed"))
+        .collect::<Vec<_>>();
 }
